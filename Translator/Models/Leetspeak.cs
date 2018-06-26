a9b8238902aeb51debe5903a62b5471819926459
Abel Trotter, Kelli Brooke and Nikki Boyd <abel@>
2018-06-26 12:00:18 -0700

    Add getters and setters for class props

--- Translator/Models/Leetspeak.cs
+++ Translator/Models/Leetspeak.cs
@@ -1,10 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Translator
 {
     public class Leetspeak
     {
-        public Leetspeak()
+        private char[] _userInput;
+        private List<char> _newPhrase;
+
+        public void SetUserInput(string userPhrase)
         {
+            char[] charList = userPhrase.ToCharArray();
+            _userInput = charList;
         }
+
+        public char[] GetUserInput()
+        {
+            return _userInput;
+        }
+
+        public void SetNewPhrase(List<char> phraseList)
+        {
+            _newPhrase = phraseList;
+        }
+
+        public bool IsLetterE(char letter)
+        {
+            if (letter == 'e')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsLetterI(char letter)
+        {
+            if (letter == 'I')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsLetterO(char letter)
+        {
+            if (letter == 'o')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsLetterT(char letter)
+        {
+            if (letter == 't')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsLetterS(char letter)
+        {
+            if (letter == 's')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
     }
 }
--- Translator/Program.cs
+++ Translator/Program.cs
@@ -6,7 +6,7 @@ namespace Translator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
