1cb8cd6abfe27b74beb88b17dbdbeec68ad2c94a
Abel Trotter, Kelli Brooke and Nikki Boyd <nikki@>
2018-06-26 11:45:23 -0700

    Create initial boolean tests for specific letters in phrase

--- Translator.Tests/ModelTests/LeetspeakTests.cs
+++ Translator.Tests/ModelTests/LeetspeakTests.cs
@@ -1,13 +1,44 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Translator;
 
 namespace Translator.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class LeetspeakTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsLetterE_TestForE_True()
         {
+            Leetspeak testLeetspeak = new Leetspeak();
+            Assert.AreEqual(true, testLeetspeak.IsLetterE('e'));
+        }
+
+        [TestMethod]
+        public void IsLetterI_TestForI_True()
+        {
+            Leetspeak testLeetspeak = new Leetspeak();
+            Assert.AreEqual(true, testLeetspeak.IsLetterI('I'));
+        }
+
+        [TestMethod]
+        public void IsLetterO_TestForO_True()
+        {
+            Leetspeak testLeetspeak = new Leetspeak();
+            Assert.AreEqual(true, testLeetspeak.IsLetterO('o'));
+        }
+
+        [TestMethod]
+        public void IsLetterT_TestForT_True()
+        {
+            Leetspeak testLeetspeak = new Leetspeak();
+            Assert.AreEqual(true, testLeetspeak.IsLetterT('t'));
+        }
+
+        [TestMethod]
+        public void IsLetterS_TestForS_True()
+        {
+            Leetspeak testLeetspeak = new Leetspeak();
+            Assert.AreEqual(true, testLeetspeak.IsLetterS('s'));
         }
     }
 }
